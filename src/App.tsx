b4a24979f4ba6e83836c5999a964ca7b8e694e0a
Avior Kahalani <aviorkahalani@gmail.com>
2025-11-16 00:01:23 +0200

    refactor: change order of imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Outlet } from 'react-router'
-import Header from '@/components/Header'
 import { Container } from '@chakra-ui/react'
-import Footer from './components/Footer'
 import { useFetchMeQuery } from './store'
+import Header from '@/components/Header'
+import Footer from './components/Footer'
 
 export default function App() {
   // fetch current user (might not be authenticated)
